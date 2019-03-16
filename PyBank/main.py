768db14198c8e813d92570186a9855d3dd019319
Mohammad Hessari <hessari@Mohammads-MacBook-Air.local>
2019-03-15 17:25:09 -0700

    source

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,63 @@
+
+Hessari
+
+import os
+import csv
+
+my_input_file = '/Users/hessari/Desktop/Budget/budget_data.csv'
+
+TotalMonths = 0
+highest = 0
+lowest = 100000000000000
+totalsum = 0
+averagemonth = 0
+rowcount = 0
+i = 0
+previous = 0
+current = 0
+running = 0
+
+
+with open(my_input_file, newline="") as csv_file:
+        csvreader = csv.reader(csv_file, delimiter=",")   
+        next(csvreader, None)
+    
+        for row in csvreader:
+        
+            if int(row[1]) >= highest:
+                highest = int(row[1])
+                highname = str(row[0])
+
+            if int(row[1])	 <= lowest:
+                lowest = int(row[1])
+                lowname = str(row[0])
+TotalMonths = TotalMonths + 1           
+totalsum = int(totalsum) + int(row[1])
+rowcount = rowcount + 1
+
+while i <= rowcount-2:
+        previous = current
+        current = int(row[1])
+        running = running + (current - previous)
+        i = i + 1
+        
+        averagemonth = running / rowcount
+        
+print("Financial Analysis")
+print("------------------------")
+print("TotalMonths: " + str(rowcount))
+print("Total Revenue: $" + str(totalsum))
+print("Average Revenue Change: $" + str(round(averagemonth,2)))
+print("Greatest Increase in Revenue: $" + str(highest) + " on " + highname)
+print("Greatest Decreased in Revenue: $" + str(lowest) + " on " + lowname)
+
+
+Financial Analysis
+------------------------
+TotalMonths: 3
+Total Revenue: $2013297
+Average Revenue Change: $223699.67
+Greatest Increase in Revenue: $1170593 on Feb-2012
+Greatest Decreased in Revenue: $-1196225 on Sep-2013
+
 #reader
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,52 @@
+Hessari
+
+import os
+import csv
+
+input_file = '/Users/hessari/Desktop/Election/election_data.csv'
+
+Candidate = []
+VoteCounter = []
+rowcount = 0
+Winner = 0
+WinnerName = ""
+i = 0
+
+with open(input_file, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    
+    for row in csvreader:
+        
+        rowcount = rowcount + 1
+        if row[2] not in Candidate:
+            Candidate.append(row[2])
+            VoteCounter.append(0)
+        else:
+            VoteCounter[Candidate.index(row[2])] = VoteCounter[Candidate.index(row[2])] + 1
+            
+Winner = max(range(len(VoteCounter)), key = lambda x: VoteCounter[x])
+WinnerName = Candidate[int(Winner)]
+
+print("Election Results are in!")
+print("-----------------")
+print("Total Votes: " + str(rowcount))
+print("--------------------")
+while i <= (len(Candidate) - 1):
+	print(Candidate[i] + ": " + str(round((VoteCounter[i]/rowcount * 100),2)) + "% (" + str(VoteCounter[i]) + ")")
+	i = i + 1
+print("---------------------")
+print("Winner: " + str(WinnerName))
+print("--------------------------")
+
+
+Election Results!
+Total Votes: 3521001
+Khan: 63.0% (2218230)
+Correy: 20.0% (704199)
+Li: 14.0% (492939)
+O'Tooley: 3.0% (105629)
+Winner: Khan
+
+
 #reader
